8be3174ff507930cf4dff3c147861516f6bb0c2f
ahmed <achau1019@gmail.com>
2021-09-26 17:25:29 -0400

    Add basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import useApplicationData from './useApplicationData';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Register from './components/Register/Register';
+import Products from './components/Products/Products';
 
 const App = () => {
   const {
@@ -11,9 +19,37 @@ const App = () => {
 ));
 
 return (<div className="App" >
-    <h1> Users </h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>
 
-    <ul> {userList} </ul>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </div >
   );
 };
