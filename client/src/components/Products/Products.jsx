9fe95e8c14b2367963969311bb454ed59a1e3eb3
no-self-required <louism.coding@gmail.com>
2021-09-30 19:53:11 -0400

    fix product layout

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,14 +1,11 @@
-import axios from 'axios';
-import React from 'react'
-import { useHistory, useLocation } from 'react-router'
-import Navbar from '../Navbar';
-import Searchbox from '../Searchbox'
-import './Products.scss'
-import Sidebar from './Sidebar';
-import SingleProduct from './SingleProduct';
-
-import { Grid } from '@mui/material';
-import { Box } from '@mui/system';
+import axios from "axios";
+import React from "react";
+import { useHistory, useLocation } from "react-router";
+import Navbar from "../Navbar";
+import Searchbox from "../Searchbox";
+import "./Products.scss";
+import Sidebar from "./Sidebar";
+import SingleProduct from "./SingleProduct";
 
 function Products() {
   const location = useLocation();
@@ -18,60 +15,61 @@ function Products() {
 
   const handleSearch = (input) => {
     const params = {
-      method: 'GET',
-      url: 'https://amazon24.p.rapidapi.com/api/product',
+      method: "GET",
+      url: "https://amazon24.p.rapidapi.com/api/product",
       params: {
-        categoryID: 'aps',
-        page: '1',
-        country: 'US',
-        filter: 'aHR0cHM6Ly93d3cuYW1hem9uLmNvbS9zP2s9aXBob25lJnJoPXBfbl9jb25kaXRpb24tdHlwZSUzQVVzZWQmZGMmcWlkPTE2MTI0MTg5NTMmcmVmPXNyX25yX3Bfbl9jb25kaXRpb24tdHlwZV8y',
-        keyword: `eco friendly ${input}`
+        categoryID: "aps",
+        page: "1",
+        country: "US",
+        filter:
+          "aHR0cHM6Ly93d3cuYW1hem9uLmNvbS9zP2s9aXBob25lJnJoPXBfbl9jb25kaXRpb24tdHlwZSUzQVVzZWQmZGMmcWlkPTE2MTI0MTg5NTMmcmVmPXNyX25yX3Bfbl9jb25kaXRpb24tdHlwZV8y",
+        keyword: `eco friendly ${input}`,
       },
       headers: {
-        'x-rapidapi-host': 'amazon24.p.rapidapi.com',
-        'x-rapidapi-key': '3cc3b85d09msh2e9a0df57326101p185615jsn2402a0cb5536'
-      }
-    }
-    
-    axios.request(params).then(function (response) {
-      history.push('/products', {props: response.data.docs})
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+        "x-rapidapi-host": "amazon24.p.rapidapi.com",
+        "x-rapidapi-key": "3cc3b85d09msh2e9a0df57326101p185615jsn2402a0cb5536",
+      },
+    };
+
+    axios
+      .request(params)
+      .then(function (response) {
+        history.push("/products", { props: response.data.docs });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
-  
+
   const products = productsArray.map((i, index) => {
     return (
-
-          <SingleProduct 
-            key={index}
-            name={i.product_title}
-            price={i.app_sale_price}
-            rating={i.evaluate_rate}
-            img={i.product_main_image_url}
-            details={i.product_detail_url}
-          />
-
-    )
-  })
+      <div className="singProduct">
+        <SingleProduct
+          key={index}
+          name={i.product_title}
+          price={i.app_sale_price}
+          rating={i.evaluate_rate}
+          img={i.product_main_image_url}
+          details={i.product_detail_url}
+        />
+      </div>
+    );
+  });
 
   return (
-    <div className='product_page'>  
+    <div className="product_page">
       <div className="column1">
         <Navbar />
-        <Searchbox rowButton={true} onClick={handleSearch}/>
-        <div className='layout'>
-          <div className="products">
-            {products}
-          </div>
+        <Searchbox rowButton={true} onClick={handleSearch} />
+        <div className="layout">
+          <div className="products">{products}</div>
         </div>
       </div>
       <div className="column2">
         <Sidebar />
-      </div> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
--- client/src/components/Products/SingleProduct.jsx
+++ client/src/components/Products/SingleProduct.jsx
@@ -1,31 +1,111 @@
-import React from 'react'
-import './SingleProduct.scss'
-import { Button, } from '@mui/material'
-import { Card } from '@mui/material'
-import { CardContent } from '@mui/material'
+import React from "react";
+import "./SingleProduct.scss";
+import { Button, Paper, Grid, Typography, CardActions } from "@mui/material";
 
-function SingleProduct({name, price, rating, img, details}) {
+import { Card } from "@mui/material";
+import { CardMedia } from "@mui/material";
+import { CardContent } from "@mui/material";
+
+function SingleProduct({ name, price, rating, img, details }) {
   return (
-    <div className='single_product'>
-      <div className="product">
-        <img src={img} alt="" />
-        <span>{`***** ${rating}`}</span>
-        <div className="info">
-          <p>{name}</p>
-          <p>{price}</p>
+    <Card sx={{ maxWidth: 345}}>
+      <div className="single_product">
+        <div className="product">
+          <CardMedia>
+            <img src={img} alt="" />
+          </CardMedia>
+          <CardContent>
+            <Typography variant="body2">{`***** ${rating}`}</Typography>
+            <div className="info">
+              <Typography variant="body2">{name}</Typography>
+              <Typography variant="body2">{price}</Typography>
+            </div>
+          </CardContent>
+          <CardActions>
+            <div className="buttons">
+              <div className="left">
+                <Button variant="outlined">
+                  <a href={details}>See Details</a>
+                </Button>
+              </div>
+              <div className="right">
+                <Button variant="outlined">Save</Button>
+                <Button variant="outlined">Share</Button>
+              </div>
+            </div>
+          </CardActions>
         </div>
-        <div className="buttons">
-          <div className="left">
-            <a href={details} >See Details</a>
-          </div>
-        <div className="right">
-          <Button variant="outlined">Save</Button>
-            <Button variant="outlined">Share</Button>
-          </div>    
-        </div> 
-      </div>   
-    </div> 
-  )
+      </div>
+    </Card>
+  );
 }
 
-export default SingleProduct
+export default SingleProduct;
+
+// function SingleProduct({ name, price, rating, img, details }) {
+//   return (
+//     <div className="single_product">
+//       <div className="product">
+//         <img src={img} alt="" />
+//         <span>{`***** ${rating}`}</span>
+//         <div className="info">
+//           <p>{name}</p>
+//           <p>{price}</p>
+//         </div>
+//         <div className="buttons">
+//           <div className="left">
+//             <Button variant="outlined">
+//               <a href={details}>See Details</a>
+//             </Button>
+//           </div>
+//           <div className="right">
+//             <Button variant="outlined">Save</Button>
+//             <Button variant="outlined">Share</Button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function SingleProduct({ name, price, rating, img, details }) {
+//   return (
+//     // <Paper sx={{ p: 2, margin: "auto", maxWidth: 500, flexGrow: 1 }}>
+//     <Grid container spacing={2}>
+//       <div className="single_product">
+//         <div className="product">
+//           <Grid item >
+//             <img src={img} alt="product-img" />
+//           </Grid>
+//           <Grid item >
+//             <div className="info">
+//               <Typography variant="body2">{name}</Typography>
+//               <Typography variant="body2">{price}</Typography>
+//             </div>
+//           </Grid>
+//           <Grid item>
+//             <Typography variant="body2">{`***** ${rating}`}</Typography>
+//           </Grid>
+//           <div className="buttons">
+//             <Grid item>
+//               <div className="left">
+//                 <Button variant="outlined">
+//                   <a href={details}>See Details</a>
+//                 </Button>
+//               </div>
+//             </Grid>
+//             <div className="right">
+//               <Grid item>
+//                 <Button variant="outlined">Save</Button>
+//               </Grid>
+//               <Grid item>
+//                 <Button variant="outlined">Share</Button>
+//               </Grid>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </Grid>
+//     // </Paper>
+//   );
+// }
