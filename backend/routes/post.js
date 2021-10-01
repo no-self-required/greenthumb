d73e5e8cc1a662e191157d3c70ab08c96c078187
Ellie Nam <ellienjuly@gmail.com>
2021-09-30 20:37:28 -0400

    Add a new post to the database

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,21 +1,28 @@
 var express = require('express');
 // const { default: Post } = require('../../client/src/components/Blog/Post');
 var router = express.Router();
+let client = require('../db/index');
+
+function newPost(post) {
+  const query = `INSERT INTO posts(title, body, likes) VALUES ('${post.title}', '${post.body}', ${post.likes})`;
+  console.log('QUERY-----', query);
+  client.query(query)
+  .then(console.log('query has been executed successfully!'))
+  .catch(e => console.log(e))
+}
 
 router.post('/', async (req, res) => {
+  const post = req.body
   const {title, body, id} = req.body
-  console.log('POST TITLE----',title);
-  console.log('POST BODY----', body);
-  console.log('POST ID----', id);
-  // const queryParams = [];
+  console.log('POST----',post);
 
-  // let queryString = 
-  // `INSERT INTO posts(title, body, username, like) VALUES
-  //   ('test', 'Some test body goes here.', 'mario', '10')`
-
-  // if(req.body) {
-  //   queryParams.push(req.body.)
-  // }
+  newPost(post);
 });
+
+router.get('/', async(req, res)=> {
+  const query = `SELECT * FROM posts;`
+  client.query(query)
+  .then(result => console.log(result))
+})
 
 module.exports = router;
